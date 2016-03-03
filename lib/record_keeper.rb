9f4f44bb134435bf9a4976297ab7a5e4861e3693
Lyonsclay <lyonsclay@yahoo.com>
2016-03-02 22:19:56 -0500

    Implement RecordKeeper methods and tests.

--- lib/record_keeper.rb
+++ lib/record_keeper.rb
@@ -1,15 +1,40 @@
+require 'csv'
+require 'date'
+
 class RecordKeeper
-  def init(file)
-    @file = file
+  attr_reader :records
+
+  def initialize(file)
+    @file = File.new("files/" + file, "w+")
+    @records = []
   end
 
-  def insert(file)
-    record = parse(file)
-
-    write_to_file
-  end
-
-  def parse(file)
+  def load(file, separator = ",")
+    file = File.absolute_path("files/" + file)
     
+    CSV.foreach(file, { col_sep: separator } ) do |row| 
+      @records << row
+    end
+  end
+
+  def order_records(col, reverse = false)
+    begin
+      Date.parse(@records[0][col])
+
+      @records.sort! { |a, b| Date.parse(a[col]) <=> Date.parse(b[col]) }
+    rescue
+      @records.sort! { |a, b| a[col] <=> b[col] }
+
+      @records.reverse! if reverse
+    end
+  end
+
+  def write_to_file
+    CSV.open(@file, "w+") do |csv| 
+      @records.each do |record| 
+        csv << record
+      end
+    end
   end
 end
+
--- spec/lib/record_keeper_spec.rb
+++ spec/lib/record_keeper_spec.rb
@@ -1,13 +1,82 @@
 require 'record_keeper'
 
-describe ".append" do
-  it "adds records to a file" do
-    io = File.readlines("./lib/pipe_delimiter.txt")
-    
-    expect(io).to eq([])
+describe RecordKeeper do
+  before do
+    file = File.new("files/comma.txt", "w+")
+    CSV.open(file, "w+") do |csv|
+      csv << ["Dekker", "Desmond", "7-16-1941", "pink"]
+      csv << ["Griffiths", "Marcia", "11-23-1949", "orange"]
+      csv << ["Donaldson", "Eric", "6-11-1947", "beige"]
+    end
+
+    file = File.new("files/colon.txt", "w+")
+    CSV.open(file, "w+", { col_sep: ";" }) do |csv| 
+      csv << ["Mowatt", "Judy", "1-1-1952", "violet"]
+      csv << ["Wailer", "Bunny", "4-10-1947", "auburn"]
+      csv << ["Marley", "Rita", "7-25-1946", "saphire"]
+    end
   end
 
-  it "does nothing" do
-    expect(file.path).to eq([])
+  after(:context) do
+    File.delete "files/comma.txt"
+    File.delete "files/colon.txt"
+  end
+
+  after(:example) do
+    begin
+      File.delete "files/test.txt"
+    rescue
+      # it's cool!
+    end
+  end
+
+  describe ".initialize" do
+    it "creates a new file" do
+      keeper = RecordKeeper.new("test.txt")
+
+      expect(keeper).to be_a RecordKeeper
+    end
+  end
+
+  describe "after hook" do
+    it "doesn't exist" do
+      file = "files/test.txt"
+
+      expect(File.exists?(file)).to eq false
+    end
+  end
+
+  describe ".load" do
+    it "loads row/columns into array" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+      
+      expect(keeper.records).to be_an Array
+      expect(keeper.records[0].count).to eq 4
+      expect(keeper.records[1][1]).to eq "Marcia"
+    end
+  end
+
+  describe ".order_records" do
+    it "sorts the rows by column value" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+
+      keeper.order_records(1, true)
+
+      expect(keeper.records[2][1]).to eq "Desmond"
+    end
+
+    it "sorts the rows by date" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+
+      keeper.order_records(2, true)
+
+      expect(keeper.records[2][1]).to eq "Marcia"
+    end
   end
 end
