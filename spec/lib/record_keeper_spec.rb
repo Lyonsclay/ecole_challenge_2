f6c2283923b77afa1e088b3708d9494a7bed791f
Lyonsclay <lyonsclay@yahoo.com>
2016-03-02 23:53:46 -0500

    Fill out tests.

--- spec/lib/record_keeper_spec.rb
+++ spec/lib/record_keeper_spec.rb
@@ -9,8 +9,8 @@ describe RecordKeeper do
       csv << ["Donaldson", "Eric", "6-11-1947", "beige"]
     end
 
-    file = File.new("files/colon.txt", "w+")
-    CSV.open(file, "w+", { col_sep: ";" }) do |csv| 
+    file = File.new("files/pipe.txt", "w+")
+    CSV.open(file, "w+", { col_sep: "|" }) do |csv| 
       csv << ["Mowatt", "Judy", "1-1-1952", "violet"]
       csv << ["Wailer", "Bunny", "4-10-1947", "auburn"]
       csv << ["Marley", "Rita", "7-25-1946", "saphire"]
@@ -19,7 +19,7 @@ describe RecordKeeper do
 
   after(:context) do
     File.delete "files/comma.txt"
-    File.delete "files/colon.txt"
+    File.delete "files/pipe.txt"
   end
 
   after(:example) do
@@ -38,14 +38,6 @@ describe RecordKeeper do
     end
   end
 
-  describe "after hook" do
-    it "doesn't exist" do
-      file = "files/test.txt"
-
-      expect(File.exists?(file)).to eq false
-    end
-  end
-
   describe ".load" do
     it "loads row/columns into array" do
       keeper = RecordKeeper.new("test.txt")
@@ -56,6 +48,15 @@ describe RecordKeeper do
       expect(keeper.records[0].count).to eq 4
       expect(keeper.records[1][1]).to eq "Marcia"
     end
+
+    it "loads multiple files" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+      keeper.load("pipe.txt")
+ 
+      expect(keeper.records.count).to eq 6
+    end
   end
 
   describe ".order_records" do
@@ -64,9 +65,9 @@ describe RecordKeeper do
 
       keeper.load("comma.txt")
 
-      keeper.order_records(1, true)
+      keeper.order_records(1)
 
-      expect(keeper.records[2][1]).to eq "Desmond"
+      expect(keeper.records[0][1]).to eq "Desmond"
     end
 
     it "sorts the rows by date" do
@@ -74,9 +75,35 @@ describe RecordKeeper do
 
       keeper.load("comma.txt")
 
-      keeper.order_records(2, true)
+      keeper.order_records(2)
 
-      expect(keeper.records[2][1]).to eq "Marcia"
+      expect(keeper.records[0][2]).to eq "11-23-1949"
+    end
+
+    it "sorts the rows in reverse" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+
+      keeper.order_records(1, true)
+
+      expect(keeper.records[2][1]).to eq "Desmond"
+    end
+  end
+
+  describe ".write_to_file" do
+    it "writes records to file" do
+      keeper = RecordKeeper.new("test.txt")
+
+      keeper.load("comma.txt")
+
+      records = keeper.records
+     
+      keeper.write_to_file
+
+      keeper.load("test.txt")
+
+      expect(keeper.records[0]).to eq records[3]
     end
   end
 end
